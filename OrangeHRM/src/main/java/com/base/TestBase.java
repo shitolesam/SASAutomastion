b0d99b604b89c4728df70d3a9964993083ce8b04
sameer03-del <shitolesameer03@gmail.com>
2022-09-13 15:13:52 +0530

    My orangeHRM project

---
+++ OrangeHRM/src/main/java/com/base/TestBase.java
@@ -0,0 +1,13 @@
+package com.base;
+
+public class TestBase {
+
+	public static void initilization()  {
+		System.setProperty("webdriver.chrome.driver","E:\\setup\\chrome\\chromedriver.exe");
+		
+		
+		
+		
+		
+	}
+}
