43afc21dacfc732a04dd7356165bac86c39aafc7
sameer03-del <shitolesameer03@gmail.com>
2022-09-13 15:28:40 +0530

    test base file updated

--- OrangeHRM/src/main/java/com/base/TestBase.java
+++ OrangeHRM/src/main/java/com/base/TestBase.java
@@ -5,7 +5,7 @@ public class TestBase {
 	public static void initilization()  {
 		System.setProperty("webdriver.chrome.driver","E:\\setup\\chrome\\chromedriver.exe");
 		
-		
+		WebDriver d = new ChromeDriver();
 		
 		
 		
